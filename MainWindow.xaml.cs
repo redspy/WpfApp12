5708ca85706151db2ae8e44408eeb6e275c253a5
한민수/시스템S/W개발Lab/CL3(Staff Engineer)/삼성메디슨 <ms007.han@samsungmedison.com>
2025-04-24 16:46:34 +0900

    디폴트 마이크 설정 추가

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,23 +13,134 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using NAudio.Wave;
+using NAudio.CoreAudioApi;
 using System.IO;
 using System.Management;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace WpfApp12
 {
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
+    [ComImport]
+    [Guid("870AF99C-171D-4F9E-AF0D-E63DF40C2BC9")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IPolicyConfig
+    {
+        [PreserveSig]
+        int GetMixFormat(string pszDeviceName, IntPtr ppFormat);
+
+        [PreserveSig]
+        int GetDeviceFormat(string pszDeviceName, int bDefault, IntPtr ppFormat);
+
+        [PreserveSig]
+        int SetDeviceFormat(string pszDeviceName, IntPtr pEndpointFormat, IntPtr MixFormat);
+
+        [PreserveSig]
+        int GetProcessingPeriod(string pszDeviceName, int bDefault, IntPtr pmftDefaultPeriod, IntPtr pmftMinimumPeriod);
+
+        [PreserveSig]
+        int SetProcessingPeriod(string pszDeviceName, IntPtr pmftPeriod);
+
+        [PreserveSig]
+        int GetShareMode(string pszDeviceName, IntPtr pMode);
+
+        [PreserveSig]
+        int SetShareMode(string pszDeviceName, IntPtr mode);
+
+        [PreserveSig]
+        int GetPropertyValue(string pszDeviceName, IntPtr key, IntPtr pv);
+
+        [PreserveSig]
+        int SetPropertyValue(string pszDeviceName, IntPtr key, IntPtr pv);
+
+        [PreserveSig]
+        int SetDefaultEndpoint(string wszDeviceId, Role eRole);
+
+        [PreserveSig]
+        int SetEndpointVisibility(string pszDeviceName, int bVisible);
+    }
+
+    [ComImport]
+    [Guid("568b9108-44bf-40b4-9006-86afe5b5a620")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IPolicyConfigVista
+    {
+        [PreserveSig]
+        int GetMixFormat(string pszDeviceName, IntPtr ppFormat);
+
+        [PreserveSig]
+        int GetDeviceFormat(string pszDeviceName, int bDefault, IntPtr ppFormat);
+
+        [PreserveSig]
+        int SetDeviceFormat(string pszDeviceName, IntPtr pEndpointFormat, IntPtr MixFormat);
+
+        [PreserveSig]
+        int GetProcessingPeriod(string pszDeviceName, int bDefault, IntPtr pmftDefaultPeriod, IntPtr pmftMinimumPeriod);
+
+        [PreserveSig]
+        int SetProcessingPeriod(string pszDeviceName, IntPtr pmftPeriod);
+
+        [PreserveSig]
+        int GetShareMode(string pszDeviceName, IntPtr pMode);
+
+        [PreserveSig]
+        int SetShareMode(string pszDeviceName, IntPtr mode);
+
+        [PreserveSig]
+        int GetPropertyValue(string pszDeviceName, IntPtr key, IntPtr pv);
+
+        [PreserveSig]
+        int SetPropertyValue(string pszDeviceName, IntPtr key, IntPtr pv);
+
+        [PreserveSig]
+        int SetDefaultEndpoint(string wszDeviceId, ERole eRole);
+
+        [PreserveSig]
+        int SetEndpointVisibility(string pszDeviceName, int bVisible);
+    }
+
+    [ComImport]
+    [Guid("294935CE-F637-4E7C-A41B-AB255460B862")]
+    internal class CPolicyConfigVistaClient
+    {
+    }
+
+    public enum ERole
+    {
+        eConsole,
+        eMultimedia,
+        eCommunications,
+        ERole_enum_count
+    }
+
     public partial class MainWindow : Window
     {
+        [DllImport("ole32.dll")]
+        private static extern int PropVariantClear(IntPtr pvar);
+
+        [DllImport("ole32.dll")]
+        private static extern int PropVariantClear(ref PROPVARIANT pvar);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct PROPVARIANT
+        {
+            public ushort vt;
+            public ushort wReserved1;
+            public ushort wReserved2;
+            public ushort wReserved3;
+            public IntPtr p;
+        }
+
         private WaveIn waveIn;
         private WaveFileWriter writer;
         private string outputFilePath = "recorded.wav";
         private List<WaveInCapabilities> microphoneDevices;
         private ManagementEventWatcher deviceWatcher;
         private SynchronizationContext uiContext;
+        private bool isSettingDefaultDevice = false;
 
         public MainWindow()
         {
@@ -66,7 +177,26 @@ namespace WpfApp12
                     
                     if (microphoneDevices.Count > 0)
                     {
-                        MicrophoneDevices.SelectedIndex = 0;
+                        // 기본 마이크 찾기
+                        var enumerator = new MMDeviceEnumerator();
+                        var defaultDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Console);
+                        string defaultDeviceName = defaultDevice.FriendlyName;
+
+                        // 기본 마이크와 일치하는 장치 찾기
+                        int defaultDeviceIndex = -1;
+                        for (int i = 0; i < microphoneDevices.Count; i++)
+                        {
+                            if (microphoneDevices[i].ProductName.Contains(defaultDeviceName))
+                            {
+                                defaultDeviceIndex = i;
+                                break;
+                            }
+                        }
+
+                        // 기본 마이크를 찾았으면 선택, 아니면 첫 번째 장치 선택
+                        isSettingDefaultDevice = true;
+                        MicrophoneDevices.SelectedIndex = defaultDeviceIndex != -1 ? defaultDeviceIndex : 0;
+                        isSettingDefaultDevice = false;
                         StartRecording.IsEnabled = true;
                         StopRecording.IsEnabled = true;
                     }
@@ -84,6 +214,36 @@ namespace WpfApp12
                     StopRecording.IsEnabled = false;
                 }
             }, null);
+        }
+
+        private void MicrophoneDevices_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (isSettingDefaultDevice || MicrophoneDevices.SelectedIndex == -1)
+                return;
+
+            try
+            {
+                var enumerator = new MMDeviceEnumerator();
+                var devices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+                var selectedDeviceName = microphoneDevices[MicrophoneDevices.SelectedIndex].ProductName;
+
+                foreach (var device in devices)
+                {
+                    if (device.FriendlyName.Contains(selectedDeviceName))
+                    {
+                        // 모든 역할에 대해 기본 장치로 설정
+                        var policyConfig = (IPolicyConfigVista)new CPolicyConfigVistaClient();
+                        policyConfig.SetDefaultEndpoint(device.ID, ERole.eConsole);
+                        policyConfig.SetDefaultEndpoint(device.ID, ERole.eMultimedia);
+                        policyConfig.SetDefaultEndpoint(device.ID, ERole.eCommunications);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"기본 장치 설정 오류: {ex.Message}");
+            }
         }
 
         private void StartDeviceWatcher()
@@ -161,5 +321,11 @@ namespace WpfApp12
             writer?.Dispose();
             base.OnClosed(e);
         }
+    }
+
+    [ComImport]
+    [Guid("870AF99C-171D-4F9E-AF0D-E63DF40C2BC9")]
+    internal class PolicyConfig
+    {
     }
 }
